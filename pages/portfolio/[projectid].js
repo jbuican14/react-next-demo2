c84d4b2d18ec800e5d2e230fbfd140907dbed077
Juti Buican <jbuican19@gmail.com>
2021-05-21 23:53:32 +0100

    Router Nextjs

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
-function PortfolioProjectPage(params) {
+import { useRouter } from 'next/router';
+
+function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query); // send a req to BE server to fetch data with
+  // id of router.query.projectid
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
